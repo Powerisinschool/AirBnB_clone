29ff6e49a0e2c580f805c631abe54174f7d4e196
Powerisinschool <toolulopeolagunju@gmail.com>
2023-02-06 18:35:34 +0100

    Task 4

--- models/base_model.py
+++ models/base_model.py
@@ -5,7 +5,16 @@ import uuid
 
 
 class BaseModel:
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
+
+        if (kwargs is not None and kwargs != {}):
+            attr = kwargs
+            attr.pop("__class__")
+            attr['created_at'] = datetime.strptime(attr['created_at'], "%Y-%m-%dT%H:%M:%S.%f")
+            attr['updated_at'] = datetime.strptime(attr['updated_at'], "%Y-%m-%dT%H:%M:%S.%f")
+            self.__dict__ = attr
+            return
+
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.__update()
