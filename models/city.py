f1ceb00da9f7f210f0e78eb1d3c24dc46f4c7599
Powerisinschool <toolulopeolagunju@gmail.com>
2023-02-07 13:24:30 +0100

    temp

---
+++ models/city.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel
+
+def City(BaseModel):
+    state_id = ""
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
---
+++ models/state.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel
+
+class State(BaseModel):
+    name = ""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
