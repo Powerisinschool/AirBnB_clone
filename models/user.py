50f7b9d66e95968ab297bc6d3a0a4e8c8073b042
Powerisinschool <toolulopeolagunju@gmail.com>
2023-02-13 08:36:47 +0100

    fix

--- models/user.py
+++ models/user.py
@@ -4,6 +4,11 @@ from models.base_model import BaseModel
 
 
 class User(BaseModel):
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
     def __init__(self, *args, **kwargs) -> None:
         self.email = ""
         self.password = ""
